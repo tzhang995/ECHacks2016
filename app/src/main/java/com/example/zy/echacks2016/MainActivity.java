40e90a7d26e21dde2b0a1356205a4857fb8317d2
Zach Yam <thepredestrian@gmail.com>
2016-11-05 02:31:36 -0400

    added UI improvements

--- app/src/main/java/com/example/zy/echacks2016/MainActivity.java
+++ app/src/main/java/com/example/zy/echacks2016/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.zy.echacks2016;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity{
+
+    public final String[] city={ "Peterborough", "Toronto", "Wateroo"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
