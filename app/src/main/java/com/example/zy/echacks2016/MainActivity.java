73973d931a2500e1cce1d2438d00b615ea9e867b
tzhang995 <tzhang995@gmail.com>
2016-11-05 03:04:34 -0400

    ListView Adapter Working

--- app/src/main/java/com/example/zy/echacks2016/MainActivity.java
+++ app/src/main/java/com/example/zy/echacks2016/MainActivity.java
@@ -3,15 +3,26 @@ package com.example.zy.echacks2016;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity{
 
-    public final String[] city={ "Peterborough", "Toronto", "Wateroo"};
+    public ArrayList<String> cityList;
+    public ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cityList.add("PeterBourogoh");
+        cityList.add("Toronto");
+        cityList.add("Waterpo");
+        mAdapter = new ArrayAdapter<String>(this,R.layout.city_listitem,cityList);
+        ListView listView = (ListView) findViewById(R.id.city_list_view);
+        listView.setAdapter(mAdapter);
     }
 
 }
